d6395a1cf88a7cc645060de956589a2ad4a798a8
Dinogabela <58051976+Dinogabela@users.noreply.github.com>
2020-03-02 20:08:14 +0100

    Add files via upload

---
+++ valjak.js
@@ -0,0 +1,7 @@
+function math() {
+	var radius = document.getElementById("radius").value;
+	var height = document.getElementById("height").value;
+	var out = document.getElementById("out");
+	out.value = Math.PI*radius*radius*height;
+}
+
